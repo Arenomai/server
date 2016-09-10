a75fae60d9e5e686afbd563251c4bb2a1c524f73
Thomas GROS <thomas.naxx@hotmail.fr>
2016-09-10 22:32:36 +0200

    Added test to get the content of the test table on the server. All working now.

--- main.cpp
+++ main.cpp
@@ -10,10 +10,11 @@ int main(int argc, char *argv[])
 {
   ifstream properties_file;
   string line, current_line, file_name, delimiter = "=", value, host, port, db_name, login, password, infos;
+  int line_number, i;
   file_name = "properties.txt";
   properties_file.open(file_name);
 
-   if(properties_file.is_open())
+   if(properties_file.is_open()) // récupération des informations de connexion
    {
       while(getline(properties_file,line))
       {
@@ -40,18 +41,27 @@ int main(int argc, char *argv[])
         cout << "Unable to open file "+file_name+" while trying to write in it";
     }
 
-    const char *conninfo;
+    const char *conninfo; // création des informations nécessaires à la db
         PGconn     *conn;
         PGresult   *res;
     infos = "hostaddr="+host+" port="+port+" dbname="+db_name+" user="+login+" password="+password;
     conninfo = infos.c_str();
-    conn = PQconnectdb(conninfo);
+    conn = PQconnectdb(conninfo); // lancement de la connexion
     if (PQstatus(conn) != CONNECTION_OK)
         {
             cout << "Connection error";
         }
     else{
-        cout << "Connection succeed";
+        cout << "Connection succeed\n";
+        cout << "Content of test table :\n";
+        res = PQexec(conn, "SELECT * FROM test"); // execution de la requête SQL
+        line_number = PQnfields(res);
+        for(i=0;i<line_number;i++)
+        {
+            printf("%-15s", PQfname(res, i));
+            cout << "\n\n";
+        }
+
     }
     return 0;
 }
