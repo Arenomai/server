435f923092a92e4f74757281d84605406fcb8594
Thomas GROS <thomas.naxx@hotmail.fr>
2016-09-10 20:41:17 +0200

    Added working connection system, but the properties parsing isn't done yet

--- main.cpp
+++ main.cpp
@@ -1,33 +1,46 @@
 #include <iostream>
 #include <fstream>
+#include <libpq/libpq-fs.h>
+#include <libpq-events.h>
+#include <libpq-fe.h>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    fstream test;
-    string line;
-    string filename;
-    filename = "test.txt";
-    test.open(filename, ios::in);
-    if(test.is_open())
+    ifstream properties_file;
+    string line, current_line, file_name, delimiter = "=", value, host, port, db_name, login, password, infos;
+    int i = 0;
+    file_name = "properties.txt";
+    properties_file.open(file_name);
+
+    if(properties_file.is_open())
     {
-    test << "that's a test bro";
-    test.close();
-    }
-    else{
-        cout << "Unable to open file "+filename+" while trying to write in it";
-    }
-    test.open(filename, ios::out);
-    if(test.is_open())
-    {
-        while(getline(test,line))
+        while(getline(properties_file,line))
         {
-            cout << line;
+            current_line = line.substr(0,line.find(delimiter));
+            value = line.substr(line.find(delimiter)+1,line.find('\0'));
         }
     }
     else{
-        cout << "Unable to open file "+filename+" while trying to write in it";
+        cout << "Unable to open file "+file_name+" while trying to write in it";
+    }
+
+    const char *conninfo;
+        PGconn     *conn;
+        PGresult   *res;
+        int         nFields;
+        int         i,
+                    j;
+    infos = "hostaddr="+host+" port="+port+" dbname="+db_name+" user="+login+" password="+password;
+    conninfo = infos.c_str();
+    conn = PQconnectdb(conninfo);
+    if (PQstatus(conn) != CONNECTION_OK)
+        {
+            cout << "Connection error";
+        }
+    else{
+        cout << "Connection succeed";
     }
     return 0;
 }
