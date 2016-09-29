10dc0738e05f06751c4067f56f5e86d8e951f90b
Celeste Wouters <elementw@openmailbox.org>
2016-09-29 15:28:08 +0200

    Implement UUID class

--- UUID.cpp
+++ UUID.cpp
@@ -1,6 +1,12 @@
-#include "uuid.hpp"
+#include "UUID.hpp"
 
-UUID::UUID()
-{
+#include <random>
 
+UUID UUID::make() {
+  static std::random_device rand;
+  static std::mt19937_64 mersenne(rand());
+  UUID uuid;
+  uuid.u64[0] = mersenne();
+  uuid.u64[1] = mersenne();
+  return uuid;
 }
--- UUID.hpp
+++ UUID.hpp
@@ -1,10 +1,33 @@
 #ifndef UUID_H
 #define UUID_H
 
+#include <cstdint>
 
-struct UUID
-{
-    UUID();
+struct UUID {
+  struct RFC {
+    uint32_t data1;
+    uint16_t data2;
+    uint16_t data3;
+    uint16_t data4;
+    uint8_t data5[6];
+  };
+  union {
+    RFC rfc;
+    uint64_t u64[2];
+    uint32_t u32[4];
+    uint16_t u16[8];
+    uint8_t u8[16];
+  };
+
+  uint8_t operator[](int idx) const {
+    return u8[idx];
+  }
+
+  uint8_t& operator[](int idx) {
+    return u8[idx];
+  }
+
+  static UUID make();
 };
 
 #endif // UUID_H
