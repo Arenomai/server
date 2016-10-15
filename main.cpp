fc8544f33b3c7ab9125c46fbc8d9116037a22048
Thomas GROS <thomasgros@live.fr>
2016-10-15 20:26:41 +0200

    Added tcp connection to the readme

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char **argv) {
         cout << results[i] << endl;
     }
     conn.exit();*/
-    TCPconnection TCPconn;
+    TCPConnection TCPconn;
     if(TCPconn.connect())
     {
     TCPconn.accept();
