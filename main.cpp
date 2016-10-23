ac30b8d938af99a67978f026dfdd41ace598699c
Celeste Wouters <elementw@openmailbox.org>
2016-10-23 21:58:18 +0200

    main: fix code style, move DB connection to top

--- main.cpp
+++ main.cpp
@@ -11,39 +11,34 @@ using namespace std;
 int main(int argc, char **argv) {
     (void) argc; (void) argv;
 
+    DatabaseConnection conn;
+    conn.connect();
+
     string result;
     TCPListener tcpl;
-    bool quit;
+    bool quit = false;
     tcpl.listen();
-    while(quit != true)
-    {
-
+    while (not quit) {
         TCPConnection tcpc = tcpl.accept();
         std::ostringstream oss;
         while (result != ";") {
             result = tcpc.read(1);
-            if(result == "µ")
-            {
+            if (result == "µ") {
                 quit = true;
+            } else {
+                cout << result;
+                oss << result;
             }
-            else{
-            cout << result;
-            oss << result;
-            }
-
         }
         cout << endl;
         oss << endl;
-        if(quit!=true)
-        {
-        DatabaseConnection conn;
-        conn.connect();
-        vector<string> results = conn.execute(oss.str());
-        for (unsigned int i = 0; i < results.size(); ++i) {
-            tcpc.write(results[i]);
-        }
-        conn.exit();
-        tcpc.disconnect();
+        if (quit != true) {
+          vector<string> results = conn.execute(oss.str());
+          for (unsigned int i = 0; i < results.size(); ++i) {
+              tcpc.write(results[i]);
+          }
+          tcpc.disconnect();
         }
     }
+    conn.exit();
 }
