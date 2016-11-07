80a58a96f13c82af87bd141256c69772bb4fee2a
Dorian Wouters <elementw@openmailbox.org>
2016-11-07 12:30:13 +0100

    Srart porting Diggler's io/net namespaces

---
+++ src/io/FileStream.hpp
@@ -0,0 +1,38 @@
+#ifndef ARN_IO_FILE_STREAM_HPP
+#define ARN_IO_FILE_STREAM_HPP
+#include <fstream>
+#include "Stream.hpp"
+
+namespace arn {
+namespace io {
+
+class InFileStream : public InStream, public SeekableStream {
+protected:
+  mutable std::ifstream strm;
+
+public:
+  InFileStream(const std::string &path);
+  ~InFileStream();
+
+  void readData(void *data, SizeT len) override;
+  PosT tell() const override;
+  void seek(OffT, Whence = Set) override;
+};
+
+class OutFileStream : public OutStream, public SeekableStream {
+protected:
+  mutable std::ofstream strm;
+
+public:
+  OutFileStream(const std::string &path);
+  ~OutFileStream();
+
+  void writeData(const void *data, SizeT len) override;
+  PosT tell() const override;
+  void seek(OffT, Whence = Set) override;
+};
+
+}
+}
+
+#endif /* ARN_IO_FILE_STREAM_HPP */
---
+++ src/io/MemoryStream.hpp
@@ -0,0 +1,63 @@
+#ifndef ARN_IO_MEMORY_STREAM_HPP
+#define ARN_IO_MEMORY_STREAM_HPP
+
+#include "Stream.hpp"
+
+namespace arn {
+namespace io {
+
+class MemoryStream : public virtual SeekableStream {
+protected:
+  byte *m_data;
+  SizeT m_length;
+  PosT m_cursor;
+
+  MemoryStream(void*, SizeT);
+  MemoryStream() : MemoryStream(nullptr, 0) {}
+
+public:
+  virtual PosT tell() const override {
+    return m_cursor;
+  }
+  virtual void seek(OffT, Whence = Set) override;
+  inline void seek(PosT pos) {
+    seek(static_cast<OffT>(pos), Whence::Set);
+  }
+
+  virtual SizeT length() const {
+    return m_length;
+  }
+  virtual const void* data() const {
+    return m_data;
+  }
+};
+
+class InMemoryStream : public virtual MemoryStream, public SizedStream, public virtual InStream {
+protected:
+  InMemoryStream() {}
+
+public:
+  InMemoryStream(const void *data, SizeT len);
+  virtual ~InMemoryStream() {}
+
+  SizeT length() const override;
+
+  virtual void readData(void *data, SizeT len) override;
+};
+
+class OutMemoryStream : public virtual MemoryStream, public virtual OutStream {
+protected:
+  SizeT m_allocated;
+  virtual void fit(SizeT);
+
+public:
+  OutMemoryStream(SizeT prealloc = 0);
+  virtual ~OutMemoryStream();
+
+  virtual void writeData(const void *data, SizeT len) override;
+};
+
+}
+}
+
+#endif /* ARN_IO_MEMORY_STREAM_HPP */
