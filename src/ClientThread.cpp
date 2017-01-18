315be1e05bf955dba07aa7eb4e8feedd10483cae
Thomas GROS <thomasgros@live.fr>
2017-01-18 13:43:06 +0100

    A space was forgotten

--- src/ClientThread.cpp
+++ src/ClientThread.cpp
@@ -31,7 +31,7 @@ void ClientThread::run() {
     time_t t = time(0);   // get time now
     struct tm * now = localtime( & t );
     try {
-        cout << now->tm_mday << "-" << (now->tm_mon + 1) << " " << now->tm_hour<<":"<<now->tm_min<<":"<<now->tm_sec << "Connected to " << tcpc.address() << ':' << tcpc.port() << endl;
+        cout << now->tm_mday << "-" << (now->tm_mon + 1) << " " << now->tm_hour<<":"<<now->tm_min<<":"<<now->tm_sec << " Connected to " << tcpc.address() << ':' << tcpc.port() << endl;
         {
             epoll_event evt;
             evt.events = EPOLLIN | EPOLLHUP | EPOLLRDHUP;
