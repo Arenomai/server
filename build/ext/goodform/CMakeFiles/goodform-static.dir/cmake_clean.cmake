3d0da8dfb456a47aec6e604155d150e567494722
Thomas GROS <thomasgros@live.fr>
2017-03-13 16:05:06 +0100

    Added cmake 3 support in travis

---
+++ build/ext/goodform/CMakeFiles/goodform-static.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/goodform-static.dir/src/io.cpp.o"
+  "CMakeFiles/goodform-static.dir/src/json.cpp.o"
+  "CMakeFiles/goodform-static.dir/src/msgpack.cpp.o"
+  "CMakeFiles/goodform-static.dir/src/variant.cpp.o"
+  "libgoodform-static.pdb"
+  "libgoodform-static.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/goodform-static.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/ext/goodform/CMakeFiles/goodform.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/goodform.dir/src/io.cpp.o"
+  "CMakeFiles/goodform.dir/src/json.cpp.o"
+  "CMakeFiles/goodform.dir/src/msgpack.cpp.o"
+  "CMakeFiles/goodform.dir/src/variant.cpp.o"
+  "../../../lib/libgoodform.pdb"
+  "../../../lib/libgoodform.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/goodform.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
